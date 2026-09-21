1877666e2613a3272ee497b67d8d56a8785ac98f
Anirban Dutta <duttashaan102@gmail.com>
2026-09-21 16:30:28 +0530

    add initial chat client

--- chat-client/src/main/java/com/anisala/chat/client/ChatClient.java
+++ chat-client/src/main/java/com/anisala/chat/client/ChatClient.java
@@ -1,5 +1,39 @@
 package com.anisala.chat.client;
 
+import com.anisala.chat.proto.ChatServiceGrpc;
+import com.anisala.chat.proto.SendMessageRequest;
+import com.anisala.chat.proto.SendMessageResponse;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class ChatClient {
 
+    public static void main(String[] args) {
+
+        ManagedChannel channel =
+                ManagedChannelBuilder
+                        .forAddress("localhost", 50051)
+                        .usePlaintext()
+                        .build();
+
+        ChatServiceGrpc.ChatServiceBlockingStub stub =
+                ChatServiceGrpc.newBlockingStub(channel);
+
+        SendMessageRequest request =
+                SendMessageRequest.newBuilder()
+                        .setSender("Alice")
+                        .setReceiver("Bob")
+                        .setMessage("Hello Bob!")
+                        .build();
+
+        SendMessageResponse response =
+                stub.sendMessage(request);
+
+        System.out.println(
+                "Server: " + response.getMessage()
+        );
+
+        channel.shutdown();
+    }
 }
